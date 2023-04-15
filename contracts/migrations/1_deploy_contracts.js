cce064c06965cbecce09ee584924321dc9f81613
Ori Ziv <oriz@starkware.co>
2023-04-15 15:02:10 +0300

    Tests working.

--- contracts/migrations/1_deploy_contracts.js
+++ contracts/migrations/1_deploy_contracts.js
@@ -1,14 +1,14 @@
-const ERC20 = artifacts.require("ERC20");
-const Dumpling = artifacts.require("Dumpling");
+// const ERC20 = artifacts.require("ERC20Mock");
+// const Dumpling = artifacts.require("Dumpling");
 
-const tokenName = "TT";
-const tokenSymbol = "TT";
-const tokenDecimals = "18";
-const tokenTotalSupply = "1000000000000000000000000000";
+// const tokenName = "TT";
+// const tokenSymbol = "TT";
+// const tokenDecimals = "18";
+// const tokenTotalSupply = "1000000000000000000000000000";
 
-module.exports = async function(deployer) {
-  deployer.then(async () => {
-    let erc20 = await deployer.deploy(ERC20, tokenName, tokenTotalSupply);
-    let dumpling = await deployer.deploy(Dumpling, erc20._address);
-  });
-};
+module.exports = async function(deployer) {};
+//   deployer.then(async () => {
+//     let erc20 = await ERC20.new("deployer.deploy(ERC20, tokenName, tokenTotalSupply);
+//     let dumpling = await deployer.deploy(Dumpling, erc20._address);
+//   });
+// };
--- contracts/test/dumpling.js
+++ contracts/test/dumpling.js
@@ -1,125 +1,71 @@
 const Dumpling = artifacts.require("Dumpling");
+const ERC20Mock = artifacts.require("ERC20Mock");
+const truffleAssert = require('truffle-assertions');
+const { time } = require("@openzeppelin/test-helpers");
 
 contract("Dumpling", (accounts) => {
-  let dumplingInstance;
+  const [lender1, lender2] = accounts;
 
-  before(async () => {
-    dumplingInstance = await Dumpling.new();
+  beforeEach(async () => {
+    this.token = await ERC20Mock.new();
+    this.dumpling = await Dumpling.new(this.token.address);
   });
 
-  it("should allow lenders to lend money", async () => {
-    const lender1 = accounts[0];
-    const lender2 = accounts[1];
-    const borrower = accounts[2];
-    const amount = web3.utils.toWei("1", "ether");
-
-    await dumplingInstance.lend(borrower, { from: lender1, value: amount });
-    await dumplingInstance.lend(borrower, { from: lender2, value: amount });
-
-    const totalLenders = await dumplingInstance.getTotalLenders();
-    assert.equal(totalLenders, 2, "Incorrect total lenders count");
-
-    const lenders = await dumplingInstance.getLenders();
-    assert.equal(lenders[0], lender1, "Incorrect lender1 address");
-    assert.equal(lenders[1], lender2, "Incorrect lender2 address");
+  it("should lend funds successfully", async () => {
+    await this.token.mint(lender1, web3.utils.toWei("20", "ether"));
+    const duration = 60 * 24 * 60 * 60; // 60 days
+    const amount = web3.utils.toWei("10", "ether");
+    await this.token.approve(this.dumpling.address, amount, { from: lender1 });
+    const result = await this.dumpling.lendFunds(duration, { from: lender1 });
+    truffleAssert.eventEmitted(result, "FundsLent", (ev) => {
+      return (
+        ev.lender === lender1 &&
+        ev.amount.toString() === amount &&
+        ev.duration.toString() === duration.toString()
+      );
+    });
   });
 
-  it("should not allow non-lenders to lend money", async () => {
-    const nonLender = accounts[3];
-    const borrower = accounts[4];
-    const amount = web3.utils.toWei("1", "ether");
-
-    try {
-      await dumplingInstance.lend(borrower, { from: nonLender, value: amount });
-    } catch (error) {
-      assert(error.message.includes("revert"), "Incorrect error message");
-    }
-
-    const totalLenders = await dumplingInstance.getTotalLenders();
-    assert.equal(totalLenders, 2, "Total lenders count should not change");
+  it("should withdraw funds successfully", async () => {
+    await this.token.mint(lender1, web3.utils.toWei("20", "ether"));
+    const duration = time.duration.days(60);
+    const amount = web3.utils.toWei("10", "ether");
+    await this.token.approve(this.dumpling.address, amount, { from: lender1 });
+    await this.dumpling.lendFunds(duration, { from: lender1 });
+    await time.increase(duration);
+    const result = await this.dumpling.withdrawFunds({ from: lender1 });
+    truffleAssert.eventEmitted(result, "FundsWithdrawn", (ev) => {
+      return ev.lender === lender1 && ev.amount.toString() !== "0";
+    });
   });
 
-  it("should not allow borrowers to lend money", async () => {
-    const lender = accounts[0];
-    const borrower = accounts[2];
-    const amount = web3.utils.toWei("1", "ether");
-
-    try {
-      await dumplingInstance.lend(borrower, { from: borrower, value: amount });
-    } catch (error) {
-      assert(error.message.includes("revert"), "Incorrect error message");
-    }
-
-    const totalLenders = await dumplingInstance.getTotalLenders();
-    assert.equal(totalLenders, 2, "Total lenders count should not change");
+  it("should revert when attempting to lend funds with an invalid duration", async () => {
+    const invalidDuration = 123456;
+    const amount = web3.utils.toWei("10", "ether");
+    await this.token.approve(this.dumpling.address, amount, { from: lender1 });
+    await truffleAssert.reverts(
+      this.dumpling.lendFunds(invalidDuration, { from: lender1 }),
+      "Invalid duration"
+    );
   });
 
-  it("should allow lenders to withdraw their funds after repayment", async () => {
-    const lender1 = accounts[0];
-    const lender2 = accounts[1];
-    const borrower = accounts[2];
-    const amount = web3.utils.toWei("1", "ether");
-
-    await dumplingInstance.repay({ from: borrower, value: amount });
-
-    const initialBalance1 = await web3.eth.getBalance(lender1);
-    const initialBalance2 = await web3.eth.getBalance(lender2);
-
-    await dumplingInstance.withdraw({ from: lender1 });
-    await dumplingInstance.withdraw({ from: lender2 });
-
-    const finalBalance1 = await web3.eth.getBalance(lender1);
-    const finalBalance2 = await web3.eth.getBalance(lender2);
-
-    assert(finalBalance1 > initialBalance1, "Incorrect balance for lender1");
-    assert(finalBalance2 > initialBalance2, "Incorrect balance for lender2");
+  it("should revert when attempting to lend funds with an existing position", async () => {
+    await this.token.mint(lender1, web3.utils.toWei("20", "ether"));
+    const duration1 = time.duration.days(60);
+    const duration2 = time.duration.days(30);
+    const amount = web3.utils.toWei("10", "ether");
+    await this.token.approve(this.dumpling.address, amount, { from: lender1 });
+    await this.dumpling.lendFunds(duration1, { from: lender1 });
+    await truffleAssert.reverts(
+      this.dumpling.lendFunds(duration2, { from: lender1 }),
+      "Existing position not yet finished"
+    );
   });
 
-  it("should not allow lenders to withdraw before repayment", async () => {
-    const lender = accounts[0];
-
-    try {
-      await dumplingInstance.withdraw({ from: lender });
-    } catch (error) {
-      assert(error.message.includes("revert"), "Incorrect error message");
-    }
-    // Check that the lender's balance remains unchanged
-    const initialBalance = await web3.eth.getBalance(lender);
-    await dumplingInstance.repay({ from: borrower, value: web3.utils.toWei("0.5", "ether") });
-    await dumplingInstance.repay({ from: borrower, value: web3.utils.toWei("0.5", "ether") });
-    const finalBalance = await web3.eth.getBalance(lender);
-    assert.equal(finalBalance, initialBalance, "Lender should not have withdrawn funds");
-});
-it("should allow borrowers to repay their debt", async () => {
-    const lender = accounts[0];
-    const borrower = accounts[2];
-    const amount = web3.utils.toWei("1", "ether");
-    await dumplingInstance.lend(borrower, { from: lender, value: amount });
-
-const initialDebt = await dumplingInstance.getDebt(borrower);
-await dumplingInstance.repay({ from: borrower, value: amount });
-const finalDebt = await dumplingInstance.getDebt(borrower);
-
-assert.equal(finalDebt.toString(), "0", "Debt should be fully repaid");
-assert(initialDebt > finalDebt, "Debt should have decreased");
-});
-
-it("should not allow borrowers to repay more than their debt", async () => {
-const lender = accounts[0];
-const borrower = accounts[2];
-const amount = web3.utils.toWei("1", "ether");
-await dumplingInstance.lend(borrower, { from: lender, value: amount });
-
-const initialDebt = await dumplingInstance.getDebt(borrower);
-
-try {
-  await dumplingInstance.repay({ from: borrower, value: web3.utils.toWei("1.5", "ether") });
-} catch (error) {
-  assert(error.message.includes("revert"), "Incorrect error message");
-}
-
-const finalDebt = await dumplingInstance.getDebt(borrower);
-
-assert.equal(finalDebt.toString(), initialDebt.toString(), "Debt should remain unchanged");
-});
+  it("should revert when attempting to withdraw funds with no funds", async () => {
+    await truffleAssert.reverts(
+      this.dumpling.withdrawFunds({ from: lender1 }),
+      "No funds to withdraw"
+    );
+  });
 });
