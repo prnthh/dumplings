4ab54c981a5ba0e0f2ea3e68361192b35a5361f8
prnth <pranith_h@yahoo.com.sg>
2023-04-15 10:24:41 +0530

    updated gitignore

---
+++ frontend/pages/_app.js
@@ -0,0 +1,32 @@
+import "@/styles/globals.css";
+import {
+  EthereumClient,
+  w3mConnectors,
+  w3mProvider,
+} from "@web3modal/ethereum";
+import { Web3Modal } from "@web3modal/react";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { arbitrum, mainnet, polygon } from "wagmi/chains";
+
+const chains = [arbitrum, mainnet, polygon];
+const projectId = "YOUR_PROJECT_ID";
+
+const { provider } = configureChains(chains, [w3mProvider({ projectId })]);
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors: w3mConnectors({ projectId, version: 1, chains }),
+  provider,
+});
+const ethereumClient = new EthereumClient(wagmiClient, chains);
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <WagmiConfig client={wagmiClient}>
+        <Component {...pageProps} />
+      </WagmiConfig>
+
+      <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
+    </>
+  );
+}
